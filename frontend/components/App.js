ccb51d1ee0a192d0730eacd7dc7c22322355b6ec
smirnakh <99379073+smirnakh@users.noreply.github.com>
2022-08-09 21:06:31 -0700

    not completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,32 @@
+import axios from 'axios'
 import React from 'react'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+   state = {
+    todos: [],
+  }
+  fetchAllTodos(){
+    axios.get(URL)
+    .then(res => this.setState({...this.state, todos: res.data}))
+    .catch(err => console.error(err))
+  }
+  componentDidMount(){
+    this.fetchAllTodos()
+  }
+  
   render() {
-    return null
+    return (
+      <div>
+      { 
+      this.state.todos.map(todo => {
+        return (
+          <div key={todo.id}>{todo.name}</div>
+        )
+      })
+      }
+      </div>
+    )
   }
 }
